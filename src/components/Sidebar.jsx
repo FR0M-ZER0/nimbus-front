703bea26c9f590962646cb2a5845d99deae3b3f1
João Suzuki <joaosuzuki98@outlook.com>
2025-09-12 09:11:23 -0300

    feat[6.4]: Adiciona efeito de ativo no botão da sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Link } from 'react-router'
 import { HouseIcon, CircuitryIcon, WarningIcon, NewspaperIcon, UsersIcon, UserIcon, GearIcon, SignOutIcon } from '@phosphor-icons/react'
 
 function Sidebar() {
+    const [activeTab, setActiveTab] = useState(0)
+
     return (
         <nav className='p-[20px] flex flex-col min-h-screen w-[300px] dark-gradient'>
             <div className='flex w-full justify-center'>
@@ -10,34 +12,34 @@ function Sidebar() {
             </div>
 
             <div className='flex flex-col mt-[50px]'>
-                <Link to={'/admin'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 0 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(0)}>
                     <HouseIcon size={28} />
                     <span className='ml-4'>Dashboard</span>
                 </Link>
-                <Link to={'/admin/stations'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/stations'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 1 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(1)}>
                     <CircuitryIcon size={28} />
                     <span className='ml-4'>Estações</span>
                 </Link>
-                <Link to={'/admin/alerts'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/alerts'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 2 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(2)}>
                     <WarningIcon size={28} />
                     <span className='ml-4'>Alertas</span>
                 </Link>
-                <Link to={'/admin/logs'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/logs'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 3 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(3)}>
                     <NewspaperIcon size={28} />
                     <span className='ml-4'>Histórico</span>
                 </Link>
-                <Link to={'/admin/users'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/users'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 4 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(4)}>
                     <UsersIcon size={28} />
                     <span className='ml-4'>Usuários</span>
                 </Link>
 
                 <p className='px-2 mt-[44px] mb-4'>Conta</p>
 
-                <Link to={'/admin/profile'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/profile'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 5 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(5)}>
                     <UserIcon size={28} />
                     <span className='ml-4'>Perfil</span>
                 </Link>
-                <Link to={'/admin/settings'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/settings'} className={`flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out ${activeTab === 6 ? 'alt-light-color-bg' : ''}`} onClick={() => setActiveTab(6)}>
                     <GearIcon size={28} />
                     <span className='ml-4'>Configurações</span>
                 </Link>

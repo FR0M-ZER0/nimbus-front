13374f305b8daadefc9cdd8b9eebd916d9c8565b
João Suzuki <joaosuzuki98@outlook.com>
2025-09-11 23:08:58 -0300

    feat[6.4]: Adiciona estrutura iniciais para as demais páginas de administração

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,37 +10,38 @@ function Sidebar() {
             </div>
 
             <div className='flex flex-col mt-[50px]'>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <HouseIcon size={28} />
                     <span className='ml-4'>Dashboard</span>
                 </Link>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/stations'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <CircuitryIcon size={28} />
                     <span className='ml-4'>Estações</span>
                 </Link>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/alerts'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <WarningIcon size={28} />
                     <span className='ml-4'>Alertas</span>
                 </Link>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/logs'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <NewspaperIcon size={28} />
                     <span className='ml-4'>Histórico</span>
                 </Link>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/users'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <UsersIcon size={28} />
                     <span className='ml-4'>Usuários</span>
                 </Link>
 
                 <p className='px-2 mt-[44px] mb-4'>Conta</p>
 
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/profile'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <UserIcon size={28} />
                     <span className='ml-4'>Perfil</span>
                 </Link>
-                <Link to={''} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
+                <Link to={'/admin/settings'} className='flex items-center mb-2 p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <GearIcon size={28} />
                     <span className='ml-4'>Configurações</span>
                 </Link>
+                {/* TODO: Remover o link, não é necessário para o sign out */}
                 <Link to={''} className='flex items-center p-2 hover:bg-[#9093B4] rounded transition-all duration-200 ease-in-out'>
                     <SignOutIcon size={28} />
                     <span className='ml-4'>Sair</span>

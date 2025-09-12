c07c59f642c94bf787ab27b1d81cbb8a125ffc2c
João Suzuki <joaosuzuki98@outlook.com>
2025-09-11 23:08:58 -0300

    feat[F0-54]: Adiciona estrutura iniciais para as demais páginas de administração

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { Routes, Route, } from 'react-router'
 // Páginas
 import AdminLayout from './pages/admin/Layout'
 import DashboardPage from './pages/admin/DashboardPage'
+import StationPage from './pages/admin/StationPage'
+import AlertPage from './pages/admin/AlertPage'
+import LogPage from './pages/admin/LogPage'
+import UsersPage from './pages/admin/UsersPage'
+import ProfilePage from './pages/admin/ProfilePage'
+import SettingsPage from './pages/admin/SettingsPage'
 
 function App() {
 	return (
@@ -11,12 +17,12 @@ function App() {
 			{/* Rotas do admin */}
 			<Route path='/admin' element={<AdminLayout />} >
 				<Route index element={<DashboardPage />} />
-				<Route path='stations' element={<DashboardPage />} />
-				<Route path='alerts' element={<DashboardPage />} />
-				<Route path='logs' element={<DashboardPage />} />
-				<Route path='users' element={<DashboardPage />} />
-				<Route path='profile' element={<DashboardPage />} />
-				<Route path='settings' element={<DashboardPage />} />
+				<Route path='stations' element={<StationPage />} />
+				<Route path='alerts' element={<AlertPage />} />
+				<Route path='logs' element={<LogPage />} />
+				<Route path='users' element={<UsersPage />} />
+				<Route path='profile' element={<ProfilePage />} />
+				<Route path='settings' element={<SettingsPage />} />
 			</Route>
 		</Routes>
 	)
---
+++ src/pages/admin/StationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function StationPage() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default StationPage

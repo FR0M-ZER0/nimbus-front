2248b788a44e240aa09a4bef1a3209d5f018243d
João Suzuki <joaosuzuki98@outlook.com>
2025-09-11 22:41:31 -0300

    feat[F0-54]: Adiciona componente topbar

---
+++ src/components/Topbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SystemSearchbar from './SystemSearchbar'
+import { UserIcon, BellIcon, GearIcon } from '@phosphor-icons/react'
+
+function Topbar({ title }) {
+    return (
+        <header className='flex py-[30px] items-center'>
+            <p>
+                { title }
+            </p>
+
+            <div className='flex items-center flex-1 justify-end'>
+                <div className='w-[300px] mr-8'>
+                    <SystemSearchbar />
+                </div>
+
+                <div className='flex'>
+                    <UserIcon size={24} className='alt-light-color-text' />
+                    <BellIcon size={24} className='alt-light-color-text mx-8' />
+                    <GearIcon size={24} className='alt-light-color-text' />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Topbar

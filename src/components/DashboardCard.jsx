8f57289a5d2e88b4b116e24578dbbe75b1a19189
João Suzuki <joaosuzuki98@outlook.com>
2025-09-13 19:09:42 -0300

    feat[6.2]: Adiciona componente de card para a dashboard

---
+++ src/components/DashboardCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import InfoCard from './InfoCard'
+
+function DashboardCard({ dataValue, title, updateDate, altData, additionMessage, icon }) {
+    return (
+        <InfoCard>
+            <div className='flex justify-between'>
+                <div className='pt-14 max-w-[360px]'>
+                    <p className='font-semibold text-6xl mb-2'>{ dataValue }{ altData ? <span className='alt-light-color-text text-4xl ml-1'>{ altData }</span> : '' }</p>
+                    <p className='text-3xl'>{ title }</p>
+                    <p className='alt-light-color-text'>Última atualização: { updateDate }</p>
+                    <p className='red-color-text'>{ additionMessage ? additionMessage : '' }</p>
+                </div>
+
+                <div className='alt-light-color-text'>
+                    { icon }
+                </div>
+            </div>
+        </InfoCard>
+    )
+}
+
+export default DashboardCard

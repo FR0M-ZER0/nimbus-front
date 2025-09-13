97d330c15d7da2940917090ef474dbd008ab7d04
João Suzuki <joaosuzuki98@outlook.com>
2025-09-12 21:02:50 -0300

    feat[F0-54]: Adiciona responsividade ao layout e aos menus

--- src/components/SystemSearchbar.jsx
+++ src/components/SystemSearchbar.jsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from '@phosphor-icons/react'
 function SystemSearchbar({ inputValue, onChange }) {
     return (
         <div className='w-full relative alt-dark-color-2-bg rounded-xl px-4 py-3' >
-            <input placeholder='Pesquisar no sistema' value={inputValue} onChange={onChange} className='w-full outline-none' />
+            <input placeholder='Pesquisar no sistema' value={inputValue} onChange={onChange} className='w-[90%] outline-none' />
             <div className='absolute right-3 top-1/2 -translate-y-1/2'>
                 <MagnifyingGlassIcon size={18} />
             </div>
--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import SystemSearchbar from './SystemSearchbar'
 import { UserIcon, BellIcon, GearIcon } from '@phosphor-icons/react'
+import HamburguerMenu from './HamburguerMenu'
 
 function Topbar({ title }) {
     return (
         <header className='flex py-[30px] items-center'>
-            <p>
+            <p className='md:mr-0 mr-2 md:inline hidden'>
                 { title }
             </p>
 
-            <div className='flex items-center flex-1 justify-end'>
-                <div className='w-[300px] mr-8'>
+            <div className='flex items-center flex-1 md:justify-end'>
+                <div className='mr-2'>
+                    <HamburguerMenu />
+                </div>
+                <div className='md:w-[300px] w-full md:mr-8 mr-2'>
                     <SystemSearchbar />
                 </div>
 
                 <div className='flex'>
                     {/* TODO: Adicionar o componente LINK a cada icone */}
-                    <UserIcon size={24} className='alt-light-color-text' />
-                    <BellIcon size={24} className='alt-light-color-text mx-8' />
-                    <GearIcon size={24} className='alt-light-color-text' />
+                    <UserIcon size={24} className='alt-light-color-text sm:block hidden' />
+                    <BellIcon size={24} className='alt-light-color-text md:mx-8 mx-2' />
+                    <GearIcon size={24} className='alt-light-color-text sm:block hidden' />
                 </div>
             </div>
         </header>

1849b2d36cd9b0bbd8e9ec4e619987ab87edf9b7
João Suzuki <joaosuzuki98@outlook.com>
2025-09-14 21:14:47 -0300

    feat[F0-55]: Adiciona componente padrão de tabela

---
+++ src/components/StationTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Table from './Table'
+
+function StationTable() {
+    return (
+        <Table title={'Estações'}>
+            
+        </Table>
+    )
+}
+
+export default StationTable
--- src/pages/admin/StationPage.jsx
+++ src/pages/admin/StationPage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import StationForm from '../../components/StationForm'
 import StationDownMap from '../../components/StationDownMap'
+import StationTable from '../../components/StationTable'
 
 function StationPage() {
+    const stations = [
+        {
+            uid: "Abc123",
+            name: "Umidade da água",
+            lat: -23.55052,
+            long: -46.633308,
+            params: "Umidade - Pluviométrico 0.25 - Vento - Fogo",
+            status: "online",
+        },
+        {
+            uid: "Tzi192",
+            name: "Pluviométrico 0.5",
+            lat: -23.55052,
+            long: -46.633308,
+            params: "Umidade - Pluviométrico 0.25 - Vento - Fogo",
+            status: "offline",
+        },
+    ]
     return (
         <div className='w-full'>
             <div className='flex'>
@@ -13,6 +32,10 @@ function StationPage() {
                 <div className='flex-1'>
                     <StationDownMap />
                 </div>
+            </div>
+
+            <div>
+                <StationTable stations={stations} />
             </div>
         </div>
         

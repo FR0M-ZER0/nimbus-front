92b7122a9d40bc6cbff319046ed46ba7bebb4bc6
João Suzuki <joaosuzuki98@outlook.com>
2025-09-16 15:07:44 -0300

    chore[F0-55]: Adiciona comentários explicativos para o hook de paginação

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,6 +1,9 @@
 import { useState, useMemo } from 'react'
 
-export function usePagination(data = [], itemsPerPage = 10) {
+/* Hook para retornar o array com os itens atuais da página (currentItems),
+* número de paǵinas (pageCount) e a função para tratar a mudança de página
+*/
+export function usePagination(data = [], itemsPerPage = 10) { // Data é o array com os itens que queremos paginar
 	const [itemOffset, setItemOffset] = useState(0)
 
 	const endOffset = itemOffset + itemsPerPage

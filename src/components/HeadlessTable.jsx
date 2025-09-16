e8d94f70e5cfa2e96fea7fd6a161b40e66468049
João Suzuki <joaosuzuki98@outlook.com>
2025-09-16 14:28:50 -0300

    refactor[F0-55]: Cria componente próprio para o paginador

--- src/components/HeadlessTable.jsx
+++ src/components/HeadlessTable.jsx
@@ -1,7 +1,6 @@
 import { React, useState } from 'react'
 import InfoCard from './InfoCard'
-import ReactPaginate from 'react-paginate'
-import { CaretRightIcon, CaretLeftIcon } from '@phosphor-icons/react'
+import Paginator from './Paginator'
 
 function HeadlessTable({ title, tableEntries, onActionBtnClick }) {
     // TODO: mudar o número de items por página quando houver os dados vindos do back
@@ -94,22 +93,7 @@ function HeadlessTable({ title, tableEntries, onActionBtnClick }) {
                         }
 					</tbody>
 				</table>
-                    <ReactPaginate
-                        containerClassName='flex space-x-4 mt-8'
-                        breakLabel='...'
-                        nextLabel={<CaretRightIcon size={28} className='cursor-pointer' />}
-                        previousLabel={<CaretLeftIcon size={28} className='cursor-pointer' />}
-                        previousClassName='h-10 w-10 p-1 alt-dark-color-3-bg rounded-lg flex justify-center items-center'
-                        nextClassName='h-10 w-10 p-1 alt-dark-color-3-bg rounded-lg flex justify-center items-center'
-                        pageClassName='h-10 w-10 rounded-lg alt-dark-color-3-bg'
-                        pageLinkClassName='w-full h-full flex justify-center items-center cursor-pointer font-semibold text-lg'
-                        activeClassName='h-11 w-11 blue-color-bg'
-                        disabledClassName='hidden'
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={5}
-                        pageCount={pageCount}
-                        renderOnZeroPageCount={null}
-                    />
+                <Paginator pageCount={pageCount} onButtonClick={handlePageClick} />
             </div>
         </InfoCard>
     )

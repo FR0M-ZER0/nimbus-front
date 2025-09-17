e364cfa25bc98f8066c5fb728db9da34b4f4bcbe
João Suzuki <joaosuzuki98@outlook.com>
2025-09-17 07:55:46 -0300

    style[6.2]: Ajusta padding inferior do modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ import { XIcon } from '@phosphor-icons/react'
 function Modal({ onClose, children }) {
     return (
         <div className='min-w-screen min-h-screen bg-black/75 fixed z-100 top-0 left-0' onClick={onClose}>
-            <div className='max-h-[700px] max-w-[1200px] fixed top-1/2 left-1/2 -translate-1/2 alt-dark-color-bg z-101 h-[700px] w-[1200px] p-12' onClick={e => e.stopPropagation()}>
+            <div className='min-h-[700px] max-w-[1200px] fixed top-1/2 left-1/2 -translate-1/2 alt-dark-color-bg z-101 h-[700px] w-[1200px] p-12 pb-22 rounded-lg' onClick={e => e.stopPropagation()}>
                 <XIcon size={28} className='absolute right-[24px] top-[24px] cursor-pointer' onClick={onClose} />
 
                 { children }

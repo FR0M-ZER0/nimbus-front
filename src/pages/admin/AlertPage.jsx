bf14de71f26b6f54a6ec92083510ae68db37479c
João Suzuki <joaosuzuki98@outlook.com>
2025-09-18 16:58:33 -0300

    feat[6.2]: Adiciona componente de últimos alertas

--- src/pages/admin/AlertPage.jsx
+++ src/pages/admin/AlertPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Calendar from '../../components/Calendar'
+import LastAlerts from '../../components/LastAlerts'
 
 function AlertPage() {
     return (
         <div className='w-full'>
-            <Calendar />
+            <div className='flex space-x-6'>
+                <div className='max-w-[1400px] w-full'>
+                    <Calendar />
+                </div>
+                <div className='flex-1'>
+                    <LastAlerts />
+                </div>
+            </div>
         </div>
     )
 }

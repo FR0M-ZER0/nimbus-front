ca0504f156a9b26b9593b9f09a3c195cbe6e797a
João Suzuki <joaosuzuki98@outlook.com>
2025-09-30 20:03:35 -0300

    feat[8.3]: Adiciona loading e feedback visual quando não há dados

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import InfoCard from './InfoCard'
 import Paginator from './Paginator'
 import Filter from './Filter'
+import loadingAnimation from '../assets/loading.gif'
 import { usePagination } from '../hooks/usePagination'
 import { motion, AnimatePresence } from 'framer-motion'
 
-function Table({ title, tableHeadEntries, tableBodyEntries }) {
+function Table({ title, tableHeadEntries, tableBodyEntries, onLoading }) {
     const { currentItems, pageCount, handlePageClick, rowAnimation } = usePagination(tableBodyEntries, 10)
 
 	return (
@@ -19,42 +20,51 @@ function Table({ title, tableHeadEntries, tableBodyEntries }) {
 					<Filter />
 				</div>
 
-				<table className='w-full'>
-					<thead>
-						{
-							tableHeadEntries.map((entry, index) => (
-								<th key={index} className='text-left alt-light-color-text pb-2 border-b-1 border-[#9093B4]'>{entry}</th>
-							))
-						}
-						<th className='text-left alt-light-color-text pb-2 border-b-1 border-[#9093B4]'>Ações</th>
-					</thead>
-
-					<tbody>
-						<AnimatePresence>
+				{onLoading ? (
+					<div className='w-full flex justify-center'>
+						<img src={loadingAnimation} alt="loading" width={160} />
+					</div>
+				) : tableBodyEntries.length === 0 ? (
+					<p className='text-center alt-light-color-text'>Não há dados disponíveis</p>
+				) : (
+					<table className='w-full'>
+						<thead>
 							{
-								currentItems.map((row) => (
-									<motion.tr
-										key={row.id || JSON.stringify(row)}
-										variants={rowAnimation}
-										initial="hidden"
-										animate="visible"
-										exit="exit"
-										transition={{ duration: 0.3 }}
-									>
-										{
-											tableHeadEntries.map((col, colIndex) => (
-												<td key={colIndex} className='pt-10 pb-4 border-b-1 border-[#9093B4]'>{row[col]}</td>
-											))
-										}
-										<td className='pt-10 pb-4 border-b-1 border-[#9093B4] '>
-											<span className='cursor-pointer hover:text-[#0000FF] transition-all duration-300'>Ver mais</span>
-										</td>
-									</motion.tr>
+								tableHeadEntries.map((entry, index) => (
+									<th key={index} className='text-left alt-light-color-text pb-2 border-b-1 border-[#9093B4]'>{entry}</th>
 								))
 							}
-						</AnimatePresence>
-					</tbody>
-				</table>
+							<th className='text-left alt-light-color-text pb-2 border-b-1 border-[#9093B4]'>Ações</th>
+						</thead>
+
+						<tbody>
+							<AnimatePresence>
+								{
+									currentItems.map((row) => (
+										<motion.tr
+											key={row.id || JSON.stringify(row)}
+											variants={rowAnimation}
+											initial="hidden"
+											animate="visible"
+											exit="exit"
+											transition={{ duration: 0.3 }}
+										>
+											{
+												tableHeadEntries.map((col, colIndex) => (
+													<td key={colIndex} className='pt-10 pb-4 border-b-1 border-[#9093B4]'>{row[col]}</td>
+												))
+											}
+											<td className='pt-10 pb-4 border-b-1 border-[#9093B4] '>
+												<span className='cursor-pointer hover:text-[#0000FF] transition-all duration-300'>Ver mais</span>
+											</td>
+										</motion.tr>
+									))
+								}
+							</AnimatePresence>
+						</tbody>
+					</table>
+				)}
+
 				<Paginator pageCount={pageCount} onButtonClick={handlePageClick} />
             </div>
         </InfoCard>

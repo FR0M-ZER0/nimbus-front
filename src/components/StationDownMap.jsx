e9700b13c2a9870a5c98d94f691ffecf7157a5b9
João Suzuki <joaosuzuki98@outlook.com>
2025-10-12 16:55:32 -0300

    feat[F0-64]: Adiciona contador dinâmico de estações on/off

--- src/components/StationDownMap.jsx
+++ src/components/StationDownMap.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-function StationDownMap() {
+function StationDownMap({ stations = [] }) {
+    const onlineCount = stations.filter(s => s.status === 'online').length
+    const offlineCount = stations.filter(s => s.status === 'offline').length
+
     return (
         <div className='flex justify-end'>
             <div className='flex flex-col mt-40 mr-12'>
-                <h3 className='text-3xl green-color-text font-semibold mb-1'>24 Estações online</h3>
-                <p className='text-5xl red-color-text font-semibold mb-4'>3 Offline</p>
+                <h3 className='text-3xl green-color-text font-semibold mb-1'>{onlineCount} Estações online</h3>
+                <p className='text-5xl red-color-text font-semibold mb-4'>{offlineCount} Offline</p>
                 <p className='mb-2'>01/03/2025 - 22:21 (Estação xyz123)</p>
                 <p className='mb-2'>01/03/2025 - 22:21 (Estação xyz123)</p>
                 <p className='mb-2'>01/03/2025 - 22:21 (Estação xyz123)</p>

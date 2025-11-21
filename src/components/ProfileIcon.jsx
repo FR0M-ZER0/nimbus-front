d2c13e34019c64384aeab2bf2a712545d9e8d37a
João Suzuki <joaosuzuki98@outlook.com>
2025-11-21 20:43:24 -0300

    style[F0-105]: Ajusta tamanho do icone

--- src/components/ProfileIcon.jsx
+++ src/components/ProfileIcon.jsx
@@ -2,8 +2,8 @@ import { UserIcon } from '@phosphor-icons/react'
 
 function ProfileIcon() {
     return (
-        <div className='dark-gradient w-36 h-36 rounded-full flex justify-center items-center'>
-            <UserIcon size={64} />
+        <div className='dark-gradient w-30 h-30 rounded-full flex justify-center items-center'>
+            <UserIcon size={52} />
         </div>
     )
 }

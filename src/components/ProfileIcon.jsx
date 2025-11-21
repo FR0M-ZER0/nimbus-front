2bf1ff18ab6338353b56a2fef22d168d62460bdd
João Suzuki <joaosuzuki98@outlook.com>
2025-11-21 17:15:58 -0300

    feat[F0-105]: Adiciona icone de perfil

---
+++ src/components/ProfileIcon.jsx
@@ -0,0 +1,11 @@
+import { UserIcon } from '@phosphor-icons/react'
+
+function ProfileIcon() {
+    return (
+        <div className='dark-gradient w-36 h-36 rounded-full flex justify-center items-center'>
+            <UserIcon size={64} />
+        </div>
+    )
+}
+
+export default ProfileIcon

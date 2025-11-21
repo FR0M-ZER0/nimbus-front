c50c3f3006e4962229b8077e0984e76ef92ef284
João Suzuki <joaosuzuki98@outlook.com>
2025-11-21 20:43:48 -0300

    feat[F0-105]: Adiciona form para mudança de dados

--- src/pages/admin/ProfilePage.jsx
+++ src/pages/admin/ProfilePage.jsx
@@ -1,13 +1,24 @@
 import ProfileIcon from "../../components/ProfileIcon"
+import UserEditForm from "../../components/UserEditForm"
 
 function ProfilePage() {
     return (
-        <div>
-            <div className="flex flex-col items-center">
+        <div className="w-full">
+            <div className="flex flex-col items-center mb-8">
                 <ProfileIcon />
                 <h1 className="text-3xl">Fulano da Silva</h1>
                 <p className="alt-light-color-text my-1">Ativo desde 09/02/2035</p>
                 <p className="red-color-text cursor-pointer">Excluir conta</p>
+            </div>
+
+            <div className="flex justify-between">
+                <div className="w-[59%]">
+                    <UserEditForm />
+                </div>
+
+                <div className="w-[39%]">
+
+                </div>
             </div>
         </div>
     )

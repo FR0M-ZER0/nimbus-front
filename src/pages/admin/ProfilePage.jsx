b4aeb9d949c710c3155210ed86e2b678a2c9254a
João Suzuki <joaosuzuki98@outlook.com>
2025-11-21 17:15:58 -0300

    feat[F0-105]: Adiciona icone de perfil

--- src/pages/admin/ProfilePage.jsx
+++ src/pages/admin/ProfilePage.jsx
@@ -1,8 +1,14 @@
+import ProfileIcon from "../../components/ProfileIcon"
 
 function ProfilePage() {
     return (
         <div>
-        
+            <div className="flex flex-col items-center">
+                <ProfileIcon />
+                <h1 className="text-3xl">Fulano da Silva</h1>
+                <p className="alt-light-color-text my-1">Ativo desde 09/02/2035</p>
+                <p className="red-color-text cursor-pointer">Excluir conta</p>
+            </div>
         </div>
     )
 }

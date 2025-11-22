79003bd2dcf000c6eeeec72d05509f954f2f6f67
João Suzuki <joaosuzuki98@outlook.com>
2025-11-22 17:38:33 -0300

    feat[F0-105]: Adiciona página inicial para relatórios

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router'
 import { useDispatch } from 'react-redux'
 
-// Páginas
 import AdminLayout from './pages/admin/Layout';
 import DashboardPage from './pages/admin/DashboardPage';
 import StationPage from './pages/admin/StationPage';
@@ -11,24 +10,18 @@ import LogPage from './pages/admin/LogPage';
 import UsersPage from './pages/admin/UsersPage';
 import ProfilePage from './pages/admin/ProfilePage';
 import SettingsPage from './pages/admin/SettingsPage';
-import LoginPage from './pages/admin/LoginPage';   // Sua página de Cadastro
-import SignInPage from './pages/admin/SignInPage'; // Sua página de Login
+import ReportsPage from './pages/admin/ReportsPage';
+import LoginPage from './pages/admin/LoginPage';
+import SignInPage from './pages/admin/SignInPage';
 
-// --- COMPONENTES DE LÓGICA DE ROTA ---
-
-// Componente para proteger rotas. Se não estiver logado, manda para o login.
 const ProtectedRoute = () => {
     const isAuthenticated = localStorage.getItem('authToken');
     return isAuthenticated ? <AdminLayout /> : <Navigate to="/signin" />;
 };
 
-// NOVO COMPONENTE: Decide a página inicial
 const InitialRedirect = () => {
-    // Verifica se a flag do primeiro acesso já existe
     const isSetupComplete = localStorage.getItem('hasAdminBeenCreated');
 
-    // Se a flag existe, o destino é a página de login.
-    // Se não existe, é o primeiro acesso, então o destino é a página de cadastro.
     const destination = isSetupComplete ? '/signin' : '/login';
     
     return <Navigate to={destination} />;
@@ -44,10 +37,7 @@ function App() {
 
     return (
         <Routes>
-            {/* ROTA PRINCIPAL: Agora aponta para nosso componente inteligente */}
             <Route path='/' element={<InitialRedirect />} />
-            
-            {/* Suas rotas públicas de cadastro e login */}
             <Route path='/login' element={<LoginPage />} />
             <Route path='/signin' element={<SignInPage />} />
 
@@ -60,6 +50,7 @@ function App() {
                 <Route path='users' element={<UsersPage />} />
                 <Route path='profile' element={<ProfilePage />} />
                 <Route path='settings' element={<SettingsPage />} />
+                <Route path='reports' element={<ReportsPage />} />
             </Route>
 
             {/* Redirecionamento para qualquer rota não encontrada */}

41db6f48cecc86a2ea6b9cd00cb596da37ebdc98
João Suzuki <joaosuzuki98@outlook.com>
2025-11-21 23:21:50 -0300

    feat[F0-105]: Adiciona tabela de estações

--- src/pages/admin/ProfilePage.jsx
+++ src/pages/admin/ProfilePage.jsx
@@ -1,7 +1,36 @@
 import ProfileIcon from "../../components/ProfileIcon"
 import UserEditForm from "../../components/UserEditForm"
+import StationsCreatedTable from "../../components/StationsCreatedTable"
+import StationImage from "../../assets/station_image.svg"
 
 function ProfilePage() {
+    const stations = [
+        {
+            image: StationImage,
+            uid: 'abc123',
+            name: 'Ulmidade da água',
+            params: ['ulmidade, luz, chuva']
+        },
+        {
+            image: StationImage,
+            uid: 'xyz123',
+            name: 'Temperatura do solo',
+            params: ['temperatura']
+        },
+        {
+            image: StationImage,
+            uid: 'uia598',
+            name: 'Velocidade do vento',
+            params: ['veloc, vento']
+        },
+        {
+            image: StationImage,
+            uid: 'fla791',
+            name: 'Nível de luz',
+            params: ['luz, chuva']
+        },
+    ]
+
     return (
         <div className="w-full pb-8">
             <div className="flex flex-col items-center mb-8">
@@ -17,7 +46,7 @@ function ProfilePage() {
                 </div>
 
                 <div className="w-[39%]">
-
+                    <StationsCreatedTable stations={stations} />
                 </div>
             </div>
         </div>

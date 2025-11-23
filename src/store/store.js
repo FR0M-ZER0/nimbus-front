a42b29cc1cdbb50bbcfceb6c6e6f0699e003f133
João Suzuki <joaosuzuki98@outlook.com>
2025-11-23 16:08:50 -0300

    feat[F0-105]: Adiciona slice para tratar da autenticação

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import logReducer from './slices/logSlice.js'
 import processingReducer from './slices/processingSlice.js'
 import summaryReducer from './slices/summarySlice.js'
 import activityReducer from './slices/activitySlice.js'
+import authSliceReducer from './slices/authSlice.js'
 import { websocketMiddleware } from './webSocketMiddleware.js'
 
 const WS_URL = 'ws://localhost:3001'
@@ -16,7 +17,8 @@ export const store = configureStore({
         log: logReducer,
         processing: processingReducer,
         summary: summaryReducer,
-        activity: activityReducer
+        activity: activityReducer,
+        auth: authSliceReducer
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(websocketMiddleware(WS_URL)),

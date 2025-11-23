6d36ca4214bd11e568ce5623b7d4af7b4ba4a790
João Suzuki <joaosuzuki98@outlook.com>
2025-11-23 19:31:30 -0300

    feat[F0-105]: Adiciona função para excluir conta

--- src/pages/admin/ProfilePage.jsx
+++ src/pages/admin/ProfilePage.jsx
@@ -2,12 +2,31 @@ import ProfileIcon from "../../components/ProfileIcon"
 import UserEditForm from "../../components/UserEditForm"
 import StationsCreatedTable from "../../components/StationsCreatedTable"
 import StationImage from "../../assets/station_image.svg"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { formatISOToBR } from "../../utils/format"
 import loadingAnimation from '../../assets/loading.gif'
+import api from "../../api/api"
+import { useNavigate } from "react-router"
+import { logout } from "../../store/slices/authSlice"
 
 function ProfilePage() {
     const user = useSelector(state => state.auth.user)
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const handleDelete = async () => {
+        try {
+            const confirmation = confirm('Deseja excluir sua conta? Esta ação não pode ser revertida')
+
+            if (confirmation) {
+                await api.delete(`/user/${user.id_usuario}`)
+                dispatch(logout())
+                navigate('/signin')
+            }
+        } catch(err) {
+            console.error(err)
+        }
+    }
 
     const stations = [
         {
@@ -45,7 +64,7 @@ function ProfilePage() {
                             <ProfileIcon />
                             <h1 className="text-3xl">{user.nome}</h1>
                             <p className="alt-light-color-text my-1">Ativo desde { formatISOToBR(user.data_criacao) }</p>
-                            <p className="red-color-text cursor-pointer">Excluir conta</p>
+                            <p className="red-color-text cursor-pointer" onClick={handleDelete}>Excluir conta</p>
                         </div>
 
                         <div className="flex justify-between">
